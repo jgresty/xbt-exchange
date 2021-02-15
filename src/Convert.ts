6032f02f770cdeeddcfc0d4c14da1afe3f54f8ce
John Gresty <git@jgresty.uk>
2021-02-15 01:04:27 +0000

    Add styling to conversion form
    A lot of styling is shared around, the next step would be to share the
    styling of components by creating some more components eg background
    cards.

--- src/Convert.ts
+++ src/Convert.ts
@@ -2,7 +2,7 @@
  * Convert - form to convert a single amount from a given currency.
  */
 
-import { LitElement, html, property } from "lit-element";
+import { LitElement, html, property, css } from "lit-element";
 import {
   assertExhaustive,
   failure,
@@ -19,7 +19,51 @@ export class Convert extends LitElement {
   currencyCodes: string[] = [];
 
   @property({ attribute: false })
+  startingValue: number = 0;
+
+  @property({ attribute: false })
+  startingCurrency: string = "";
+
+  @property({ attribute: false })
   result: RemoteData<number> = notAsked;
+
+  static styles = css`
+    :host {
+      display: flex;
+      margin: 2rem 0rem;
+      width: 50%;
+      min-width: 100px;
+      flex-direction: column;
+      align-items: center;
+      color: #353535;
+      background-color: white;
+      filter: drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2));
+      padding: 1rem 3rem;
+      border-radius: 0.7rem;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+    label {
+      margin-top: 1rem;
+    }
+    input[type="submit"] {
+      margin-top: 1.5rem;
+    }
+
+    span {
+      margin-top: 1rem;
+      font-size: 24pt;
+      color: #284b63;
+    }
+
+    span.error {
+      color: red;
+    }
+  `;
 
   private handleSubmit = async (event: Event) => {
     event.preventDefault();
@@ -52,6 +96,8 @@ export class Convert extends LitElement {
     if (response.ok) {
       const body = await response.text();
       this.result = success(Number.parseFloat(body));
+      this.startingValue = parsedAmount;
+      this.startingCurrency = currency;
     } else {
       this.result = failure("API request failed");
     }
@@ -62,11 +108,23 @@ export class Convert extends LitElement {
       case "NotAsked":
         return html``;
       case "Loading":
-        return html`Loading`;
+        return html`<span>Loading</span>`;
       case "Failure":
-        return html`Error: ${this.result.error}`;
+        return html`<span class="error">Error: ${this.result.error}</span>`;
       case "Success":
-        return html`${this.result.data}`;
+        return html`<span
+          >${this.startingValue.toLocaleString(undefined, {
+            style: "currency",
+            currency: this.startingCurrency,
+            minimumSignificantDigits: 5,
+          })}
+          =
+          ${this.result.data.toLocaleString(undefined, {
+            style: "currency",
+            currency: "XBT",
+            minimumSignificantDigits: 5,
+          })}</span
+        >`;
       default:
         assertExhaustive(this.result);
         return html``;
@@ -85,6 +143,6 @@ export class Convert extends LitElement {
         <input type="number" id="amount" name="amount" />
         <input type="submit" value="Convert" />
       </form>
-      <span>${this.renderResult()}</span>`;
+      ${this.renderResult()}`;
   }
 }
